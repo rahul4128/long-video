ebca1185feb9bc2921fc0e5b09967274c289f2e8
rahul4128 <82466264+rahul4128@users.noreply.github.com>
2026-09-02 12:03:17 +0530

    Modify Remotion config for Chromium settings
    Updated Remotion configuration to set Chromium renderer and flags.

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,4 +1,11 @@
-import { Config } from '@remotion/cli/config';
+import { Config } from "@remotion/cli/config";
 
-Config.setVideoImageFormat('jpeg');
+Config.setChromiumOpenGlRenderer("angle");
+Config.setBrowserExecutable(null);
 Config.setOverwriteOutput(true);
+Config.setChromiumFlags([
+  "--no-sandbox",
+  "--disable-setuid-sandbox",
+  "--disable-dev-shm-usage",
+  "--disable-gpu",
+]);
